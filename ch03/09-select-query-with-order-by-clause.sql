f13254e7fd6897c667f436728967e855be336d6d
David Stevenson <drstevenson1958@yahoo.com>
2025-12-31 14:50:44 -0500

    Chapter 3: Select Query with Order By clause

---
+++ ch03/09-select-query-with-order-by-clause.sql
@@ -0,0 +1,8 @@
+SELECT c.CustomerNumber, c.Name, c.BillingCity, ih.InvoiceNumber, ih.TotalAmount, ld.Discount
+FROM dbo.Customer c
+	INNER JOIN dbo.InvoiceHeader ih
+		ON c.CustomerNumber = ih.CustomerNumber 
+	LEFT OUTER JOIN dbo.LoyaltyDiscount ld
+		ON c.CustomerNumber = ld.CustomerNumber 
+WHERE c.BillingCity = 'Axelburg' AND c.Name > 'C' 
+ORDER BY c.Name DESC, ih.InvoiceNumber ;
