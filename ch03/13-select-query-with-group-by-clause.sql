09466cab2b593930308c3d82c5a0acf0d62ee0ec
David Stevenson <drstevenson1958@yahoo.com>
2025-12-31 15:01:41 -0500

    Chapter 3: Select Query with GROUP BY clause

---
+++ ch03/13-select-query-with-group-by-clause.sql
@@ -0,0 +1,11 @@
+SELECT c.CustomerNumber, c.Name, c.BillingCity, COUNT(ih.InvoiceNumber) AS NumberOfInvoices
+	, SUM(ih.TotalAmount) AS TotalAmount, ld.Discount
+	, SUM(ih.TotalAmount - (ih.TotalAmount * ISNULL(ld.Discount, 0.00))) AS DiscountedTotalAmount
+FROM dbo.Customer c
+	INNER JOIN dbo.InvoiceHeader ih
+		ON c.CustomerNumber = ih.CustomerNumber 
+	LEFT OUTER JOIN dbo.LoyaltyDiscount ld
+		ON c.CustomerNumber = ld.CustomerNumber 
+WHERE c.BillingCity = 'Axelburg' AND c.Name > 'C' 
+GROUP BY c.CustomerNumber, c.Name, c.BillingCity, ld.Discount
+ORDER BY c.Name DESC ;
